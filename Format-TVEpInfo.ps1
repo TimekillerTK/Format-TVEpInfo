c68e86662a91163fb483ecbfad7b52994f404ef0
TimekillerTK <erwartungen@protonmail.com>
2020-09-21 08:40:55 +0200

    working version with most features implemented

--- Format-TVEpInfo.ps1
+++ Format-TVEpInfo.ps1
@@ -30,26 +30,23 @@ function Format-TVEpInfo {
 
         # Will remove NTFS illegal characters / ? < > \ : * | " from the Episode Title 
         [switch]
-        $RemoveIllegalChar
+        $RemoveIllegalChar,
+
+        # Will include season info
+        [switch]
+        $Season,
+
+        # Will include year info
+        [switch]
+        $Year
     )
     PROCESS {
-            
-        # fixed the issue with PSCustomObject type, this was a TYPE issue
-        #Write-output $InputObject
-
-
-        # Switch for adding a leading zeroes to episode number moved to Get-TVEpList
 
         # Loop is accepting values one by one, which is why object count will always be 1
         # This foreach is probably useless, get rid of it later      
         foreach ($object in $InputObject) {
 
-            # Setting variables, kind of optional, but will avoid changing the object in the pipeline permanently
             $eptitle = $object.EpTitle
-            $season = $object.Season
-            $showtitle = $object.ShowTitle
-            $date = $object.Date
-            $episode = $object.Episode
 
             if ($PSBoundParameters.ContainsKey('RemoveIllegalChar')) {
                     
@@ -148,12 +145,31 @@ function Format-TVEpInfo {
                 } #function
 
                 # This is what will swap the number itself
-                $eptitle = $eptitle -replace "[0-9]", #-replace: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7
+                # The $Matches var returns the matches that were matched by -match
+                $eptitle -match "[0-9]" | Out-Null
+                $eptitle = $eptitle -replace "[0-9]" ,{ConvertTo-RomanNumeral $Matches[0]}
                 
             } #if
+            
+            # setting variables for the output string (formatted text)
+            $showtitle = $object.ShowTitle
+            $episode = " - Episode " + $object.EpisodeZeroed
+            $eptitle = " - " + $eptitle
 
+            if ($PSBoundParameters.ContainsKey('Season')) {
 
-            $output = "$($showtitle) - Episode $($episode) - $($eptitle)"
+                $seasonvar = " - Season " + $object.Season
+
+            } #if
+
+            if ($PSBoundParameters.ContainsKey('Year')) {
+                    
+                $datevar = " - ($($object.Date))"
+
+            } #if
+
+            # Combines all the variables to a final string
+            $output = $showtitle + $seasonvar + $datevar + $episode + $eptitle
 
             # This block deals with the Extension parameter
             if ($PSBoundParameters.ContainsKey('Extension')) {
@@ -164,8 +180,6 @@ function Format-TVEpInfo {
         
         } #foreach
 
-
-        
         return $output
 
     } #PROCESS
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Format-TVEpInfo
+Intended to format input objects into a string suitable for file names.
+* Example output: `Show Title - Season 2 - Episode 034 - Episode Title.txt`
+
+## How to use
+* `Import-CSV c:\file.csv | Format-TVEpInfo -Extension txt`
+* `Get-TVEpList https://url.com/show/season/1 | Format-TVEpInfo -Extension txt`
+* Check `Get-Help Format-TVEpInfo`
+
+## Todo
+- Current way of passing parameters to the cmdlet is a bit strange - should accept a custom format akin to `Get-Date -Format "dddd MM/dd/yyyy HH:mm K"` for displaying Season/Episode/etc
+- `ConvertTo-RomanNumeral` function has too many if statements, need to check if it can be turned into a switch or programmed more efficiently.
